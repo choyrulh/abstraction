3491bba58c37037cc36d8a63f18ef82f24471af1
choyrulh <choirul.humam@students.amikom.ac.id>
2020-06-13 12:22:01 +0700

    inisialisasi project

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+namespace Encapsulation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Mahasiswa mhs = new Mahasiswa();
+            mhs.Nim = "2810";
+            mhs.Nama = "Apryan Rizky Saputra";
+            mhs.Registrasi();
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Programm.cs
@@ -0,0 +1,30 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+namespace Inheritance
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Mahasiswa mhs = new Mahasiswa();
+            mhs.Nama = "Apryan Rizky Saputra";
+            mhs.Nim = "19.11.2810";
+            mhs.Alamat = "Jl.P.Puger 1 no 1 puyeran,Maguwoharjo";
+            mhs.Email = "apryan.s@students.amikom.ac.id";
+            mhs.CetakInformasi();
+
+            Dosen dosen = new Dosen();
+            dosen.Nama = "Kamarudin";
+            dosen.Nik = "190302123";
+            dosen.NPWP = "11223344234";
+            dosen.Alamat = "Jl.Wonosari";
+            dosen.Email = "Kamarudin@amikom.ac.id";
+            dosen.Nohp = "0813  12334";
+            dosen.CetakInformasi();
+        }
+    }
+}
